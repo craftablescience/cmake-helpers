95ed2416fbbe1c73f0380605b1112be7f393d87e
Laura <me@craftablescience.info>
2025-03-11 00:28:16 -0400

    feat: ConfigureDefaults.cmake

---
+++ ConfigureDefaults.cmake
@@ -0,0 +1,16 @@
+# run this directly after project() in the top level CMakeLists.txt
+macro(cs_configure_defaults LTO_OPTION)
+    set(CMAKE_SKIP_BUILD_RPATH OFF)
+    set(CMAKE_BUILD_RPATH_USE_ORIGIN ON)
+    set(CMAKE_INSTALL_RPATH $ORIGIN)
+
+    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+    if(LTO_OPTION)
+        include(CheckIPOSupported)
+        if(${LTO_OPTION})
+            check_ipo_supported(RESULT ${LTO_OPTION})
+        endif()
+        set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ${${LTO_OPTION}})
+    endif()
+endmacro()
