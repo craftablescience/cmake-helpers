749fb290ce01df71017d527fef5ecb26d603751f
Laura <me@craftablescience.info>
2025-03-11 00:20:58 -0400

    feat: VisualStudio.cmake

---
+++ VisualStudio.cmake
@@ -0,0 +1,5 @@
+# run this directly after project() in the top level CMakeLists.txt
+macro(cs_setup_vs_defaults)
+    set(CMAKE_FOLDER "Thirdparty" CACHE INTERNAL "" FORCE)
+    set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+endmacro()
