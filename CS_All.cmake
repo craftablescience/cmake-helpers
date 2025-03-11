c3af70c304c048bef1ee3c53f9030f090bd0bd1e
Laura <me@craftablescience.info>
2025-03-11 01:20:21 -0400

    chore: add CS_IncludeDirectory to CS_All

--- CS_All.cmake
+++ CS_All.cmake
@@ -1,4 +1,7 @@
+include_guard(GLOBAL)
+
 # assume CMAKE_MODULE_PATH is properly modified by now
 include(CS_ConfigureDefaults)
 include(CS_ConfigureTarget)
+include(CS_IncludeDirectory)
 include(CS_VisualStudio)
