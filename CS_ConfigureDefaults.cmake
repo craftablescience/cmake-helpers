fba590d88a5971afac337e570576f2072d3a89df
Laura <me@craftablescience.info>
2025-03-15 23:09:14 -0400

    fix: set rpath to $ORIGIN for standalone builds in CS_ConfigureDefaults.cmake

--- CS_ConfigureDefaults.cmake
+++ CS_ConfigureDefaults.cmake
@@ -2,8 +2,9 @@ include_guard(GLOBAL)
 
 # run this directly after project() in the top level CMakeLists.txt
 macro(cs_configure_defaults LTO_OPTION)
-    set(CMAKE_SKIP_BUILD_RPATH OFF)
+    set(CMAKE_SKIP_RPATH OFF)
     set(CMAKE_BUILD_RPATH_USE_ORIGIN ON)
+    set(CMAKE_BUILD_RPATH $ORIGIN)
     set(CMAKE_INSTALL_RPATH $ORIGIN)
 
     set(CMAKE_POSITION_INDEPENDENT_CODE ON)
