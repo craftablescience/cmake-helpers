a22e0998c61aaebe8a52c062eb52eb08d8b8223d
Laura L. <me@craftablescience.info>
2025-03-27 23:40:33 -0400

    feat: add STARTUP_PROJECT parameter to CS_VisualStudio.cmake

--- CS_VisualStudio.cmake
+++ CS_VisualStudio.cmake
@@ -2,6 +2,16 @@ include_guard(GLOBAL)
 
 # run this directly after project() in the top level CMakeLists.txt
 macro(cs_setup_vs_defaults)
-    set(CMAKE_FOLDER "Thirdparty" CACHE INTERNAL "" FORCE)
-    set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+    if(WIN32)
+        set(CMAKE_FOLDER "Thirdparty" CACHE INTERNAL "" FORCE)
+        set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+
+        # set startup project if specified
+        cmake_parse_arguments(__CS_DETAIL_VS_DEFAULTS_OPT "" "STARTUP_PROJECT" "" "${ARGN}")
+        if(__CS_DETAIL_VS_DEFAULTS_OPT_STARTUP_PROJECT)
+            set_property(
+                    DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                    PROPERTY VS_STARTUP_PROJECT "${__CS_DETAIL_VS_DEFAULTS_OPT_STARTUP_PROJECT}")
+        endif()
+    endif()
 endmacro()
